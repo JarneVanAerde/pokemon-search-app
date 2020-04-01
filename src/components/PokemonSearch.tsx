f4fcb7829dfcafb79ddd3368a1d1e90e7bbbb3ab
Jarne Van Aerde <jarne.vanaerde@hotmail.com>
2020-04-01 12:15:22 +0200

    cleaned up the search component

--- src/components/PokemonSearch.tsx
+++ src/components/PokemonSearch.tsx
@@ -1,22 +1,75 @@
-import React, { Component } from "react";
+import React, { Component, ReactElement } from "react";
 import { SearchState } from "../models/SearchState.interface";
 import PokemonResult from "./PokemonResult";
 
+/**
+ * This component is used to handle the search functionality of the application.
+ */
 export class PokemonSearch extends Component<{}, SearchState> {
-  pokemonRef: React.RefObject<HTMLInputElement>;
+  
+  /**
+   * The reference to the input field used for the search.
+   * This is an uncontrolled way of handeling the input event.
+   */
+  private pokemonRef: React.RefObject<HTMLInputElement>;
 
   constructor(props: any) {
     super(props);
-    this.state = {
-      error: false,
-      pokemon: null
-    };
+    this.state = { error: false, pokemon: null };
     this.pokemonRef = React.createRef();
   }
 
-  onSearchClick = () => {
+  /**
+   * Renders the component.
+   */
+  public render() {
+
+    // Determine the markup for rendering.
+    const resultMarkup = this.determineMarkup();
+
+    // Render the component.
+    return (
+      <div>
+        <input ref={this.pokemonRef} />
+        <button onClick={this.onSearchClick} className="my-button">
+          Search
+        </button>
+
+        {resultMarkup}
+      </div>
+    );
+  }
+
+  /**
+   * Determines the markup for rendering based on the state of this component.
+   *
+   * @returns The corresponding HTML based on the component state.
+   */
+  private determineMarkup(): ReactElement {
+
+    // Deconstruct the state of this component.
+    const { error, pokemon } = this.state;
+
+    // Determine the markup for this component based on the state.
+    if (error) {
+      return <p>Pokemon not found, please try again</p>;
+    } else if (this.state.pokemon) {
+      return <PokemonResult {...pokemon} />;
+    } else {
+      return <p>Type something, click search, and watch what happens!</p>;
+    }
+  }
+
+  /**
+   * @type EventHandler
+   * Calls the pokemon API when the user clicks search.
+   */
+  private readonly onSearchClick = (): void => {
+
+    // Get the value from the input reference.
     const inputValue = this.pokemonRef.current.value;
 
+    // Call the API.
     fetch(`https://pokeapi.co/api/v2/pokemon/${inputValue}/`).then(result => {
       if (result.status !== 200) {
         this.setState({ error: true });
@@ -36,28 +89,6 @@ export class PokemonSearch extends Component<{}, SearchState> {
       });
     });
   };
-
-  render() {
-    const { error, pokemon } = this.state;
-
-    let resultMarkup;
-    if (error) {
-      resultMarkup = <p>Pokemon not found, please try again</p>;
-    } else if (this.state.pokemon) {
-      resultMarkup = <PokemonResult {...pokemon} />;
-    }
-
-    return (
-      <div>
-        <input type="text" ref={this.pokemonRef} />
-        <button onClick={this.onSearchClick} className="my-button">
-          Search
-        </button>
-
-        {resultMarkup}
-      </div>
-    );
-  }
 }
 
 export default PokemonSearch;
--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -5,7 +5,7 @@ import { User } from "../models/User.interface";
  * This component will be used to display the information about the user.
  */
 export class UserInfo extends Component<User> {
-    
+  
   /**
    * Renders the component.
    */
